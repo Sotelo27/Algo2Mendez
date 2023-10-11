408be4971650d87d771bd4d160e1a6aab05a86b9
martin <lmsotelo@fi.uba.ar>
2023-10-11 20:56:50 -0300

    inicio ejercicio 3

--- RPL/GUIA 2/2.9-Ajustando la Fuerza/solucion.c
+++ RPL/GUIA 2/2.9-Ajustando la Fuerza/solucion.c
@@ -3,8 +3,7 @@
 Exprimamos este ejercicio un poquito más. 
 Resulta que ahora nuestro pokemon no posee una variable float fuerza, sino que posee float* fuerza. 
 Alguien más ya realizó los cálculos necesarios para la nueva fuerza, y se la recibe por parámetro.
- Se pide actualizar la fuerza del pokemon con la pasada.
-
+Se pide actualizar la fuerza del pokemon con la pasada.
 No te preocupes por temas de memoria dinámica, 
 nosotros nos encargamos de todo detrás de escena ;)
 
@@ -13,5 +12,7 @@ Si se recibe una, no se modifica la fuerza del pokemon.*/
 #include "solucion.h"
 
 void actualizar_fuerza(pokemon_t* pokemon, float* nueva_fuerza){
-    //tu código
+    if(*nueva_fuerza >= 0){
+        pokemon->fuerza = nueva_fuerza;
+    }
 }
